0253119fd77e4190d11cc5d387ace956cc2129de
shakibdshy <shakibdshy@gmail.com>
2025-10-25 14:33:57 +0600

    Add 1. hello-world.js

---
+++ 1. hello-world.js
@@ -0,0 +1,29 @@
+console.log("Hello, World!");
+console.log("I am learning JavaScript.");
+console.log("This is a simple script.");
+console.log("Goodbye, World!");
+console.log('Goodbye, World!!!!!!');
+
+console.log(45);
+console.log(2025);
+console.log(10 + 5);
+console.log(10 * 5);
+console.log(10 / 5);
+console.log(10 - 5);
+console.log(true);
+console.log(false);
+
+console.log("Goodbye, World!", 2025, "I am learning JavaScript.", 10 + 5);
+
+// Mistakes #1 Forgetting the quotations marks
+// console.log(Hello, World!);
+
+// Mistakes #2 Spelling errors
+// consol.log("Hello, World!");
+// console.log()("Hello, World!");
+// console.log("true");
+
+// Mistake #3 Not opening the console
+// Mistake #4 Forgetting the boolean values are not in quotations
+// console.log(true);
+// console.log(false);
