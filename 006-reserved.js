353406f9a7977dfdae49aab791544950d5bc578f
shakibdshy <shakibdshy@gmail.com>
2025-10-26 18:41:22 +0600

    Add 006-reserved.js: Introduce JavaScript reserved words, categories, examples, and best practices

---
+++ 006-reserved.js
@@ -0,0 +1,64 @@
+// WHAT ARE RESERVED WORDS?
+// Examples
+// let let = "hello"
+// let function = 25
+
+// CATEGORIES OF RESERVED WORDS IN JAVASCRIPT:-
+// - Declaration Keywords
+    // var, let, const, 
+    // function, 
+    // class
+
+// - Control Flow Keywords
+    // if, else, 
+    // switch, case, default, 
+    // for, while, do,
+    //  break, continue, 
+    // return, 
+
+// - Value Keywords
+    // true, false, 
+    // null, 
+    // undefined,
+
+// Module and Import/Export Keywords
+    // import, export, 
+    // from, as
+
+// - Other Keywords
+    // this, new, delete, typeof, void,  
+    // try, catch, finally, throw
+    // in, of, instanceof,
+    // with, yield, async, await
+
+// 2. FUTURE RESERVED WORDS:
+//    Examples: enum, implements, interface, package, private, protected, public, static
+
+// 3. PRACTICAL TIPS.
+
+// 4. BEST PRACTICES.
+// let userName = "John"
+// let useAge = 35;
+// const isLoggedIn = true;
+
+// const calculateTotal = function (){}
+
+// const studentClass = "10th Grade"
+
+// 5. COMMON MISTAKES.
+    // let return = 10
+    // let if = true
+
+    // let Let = "Hello";
+    // const Return = 100;
+
+// 6. REAL-WORLD EXAMPLE
+// ❌ Trying to use reserved words
+// let class = "10th Grade";      // Error!
+// let default = "Not Selected";  // Error!
+// let new = true;                // Error!
+
+// // ✅ Proper naming
+// let studentClass = "10th Grade";
+// let defaultValue = "Not Selected";
+// let isNewStudent = true;
